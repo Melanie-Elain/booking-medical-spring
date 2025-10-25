f2b6c757a4159ab4439f854281b55916b2318a0b
Melanie-Elain <baohan.tbh0406@gmail.com>
2025-10-25 14:10:59 +0700

    fix homepage

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import Header from "./Header";
 import HomeBanner from "./HomeBanner";
 import HomeSpecialty from "./HomeSpecialty";
 import HomeDoctor from "./HomeDoctor";
 import HomeHospital from "./HomeHospital";
+import SuggestionSection from "./SuggestionSection";
+import DownloadApp from "./DownloadApp";
 import HomeFooter from "./HomeFooter";
+
 
 const HomePage = () => {
   return (
     <div>
+      <Header />  
       <HomeBanner />
       <HomeSpecialty />
       <HomeDoctor />
       <HomeHospital />
+      <SuggestionSection />
+      <DownloadApp />
       <HomeFooter />
     </div>
   );
